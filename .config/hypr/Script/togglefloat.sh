c2aa4d58f0cd16979ebdcc8b9a764c7a6f9e9ee1
playrough <106418248+playrough@users.noreply.github.com>
2025-11-28 00:20:52 +0700

    Update dotfiles

--- .config/hypr/Script/togglefloat.sh
+++ .config/hypr/Script/togglefloat.sh
@@ -2,7 +2,7 @@
 
 hyprctl dispatch togglefloating
 
-if hyprctl activewindow | grep -Eq "class: (kitty|org.kde.dolphin|nemo)" && \
+if hyprctl activewindow | grep -Eq "class: (kitty|org.kde.dolphin|nemo|Code)" && \
     hyprctl activewindow | grep -q "floating: 1";
 then
     hyprctl dispatch resizeactive exact 890 520

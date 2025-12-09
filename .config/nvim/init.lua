9791c3034594042369259a17e09699ca8d35221f
playrough <106418248+playrough@users.noreply.github.com>
2025-12-10 03:00:21 +0700

    Update dotfiles

--- .config/nvim/init.lua
+++ .config/nvim/init.lua
@@ -314,6 +314,8 @@ require('lazy').setup({
     },
   },
 
+  { 'brenoprata10/nvim-highlight-colors', opts = {} },
+
   -- NOTE: Plugins can also be configured to run Lua code when they are loaded.
   --
   -- This is often very useful to both group configuration, as well as handle

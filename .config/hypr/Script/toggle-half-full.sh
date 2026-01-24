c4458ab20d84147084dca1235f66e13e064779aa
playrough <106418248+playrough@users.noreply.github.com>
2026-01-24 21:26:03 +0700

    Update dotfiles

--- .config/hypr/Script/toggle-half-full.sh
+++ .config/hypr/Script/toggle-half-full.sh
@@ -5,6 +5,6 @@ hyprctl dispatch togglefloating
 
 # nếu sau togglefloating cửa sổ đang floating thì resize + center
 if hyprctl activewindow | grep -q "floating: 1"; then
-    hyprctl dispatch resizeactive exact 1600 900
+    hyprctl dispatch resizeactive exact 1800 900
     hyprctl dispatch centerwindow
 fi
